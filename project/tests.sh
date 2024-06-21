5655b941e878999e9d591ee6b9940265a17f087b
Jagruti-Godambe <69747818+Jagruti-Godambe@users.noreply.github.com>
2024-06-21 17:09:01 +0200

    tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip3 install --upgrade pip
-pip3 install -r /Users/jagrutigodambe/MADE/project/requirements.txt
-python3 /Users/jagrutigodambe/MADE/project/pipeline.py
-python -m unittest ./Users/jagrutigodambe/MADE/project/test_pipeline.py
+pip3 install -r ./project/requirements.txt
+python3 ./project/pipeline.py
+python -m unittest ./project/test_pipeline.py
